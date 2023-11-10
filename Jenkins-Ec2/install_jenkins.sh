027f6dafb834a8426e8c6bd4cf3ef620621b735d
Jaro233 <jaroslaw.waliczek@vp.pl>
2023-11-10 10:15:26 +0100

    update

--- Jenkins-Ec2/install_jenkins.sh
+++ Jenkins-Ec2/install_jenkins.sh
@@ -3,7 +3,8 @@ exec > >(tee -i /var/log/user-data.log)
 exec 2>&1
 sudo apt update -y
 sudo apt install software-properties-common
-sudo add-apt-repository --yes --update ppa:ansible/ansible
+sudo apt-add-repository -y ppa:ansible/ansible
+sudo apt-get update -y
 sudo apt install ansible -y
 sudo apt install git -y 
 mkdir Ansible && cd Ansible
